d44e564bac98fe040d3c1bad71407058899d4fb9
Simon Paulsson <simon@paulsson.one>
2022-05-26 16:40:51 +0200

    fix

--- atomic_radius/relativity.py
+++ atomic_radius/relativity.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-from .constants import *
+import atomic_radius.constants as const
 
 def relativistic_velocity(E, m):
 	"""
 	Return the relativistic velocity of a particule with energy E and mass m
 	"""
-	return c * np.sqrt(1 - m**2 * c**4 / E**2)
+	return const.c * np.sqrt(1 - m**2 * const.c**4 / E**2)
 
 def relativistic_momentum(E, m):
 	"""
 	Return the relativistic momentum of a particule with energy E and mass m
 	"""
-	return np.sqrt(E**2 / c**2 - m**2 * c**2)
+	return np.sqrt(E**2 / const.c**2 - m**2 * const.c**2)
